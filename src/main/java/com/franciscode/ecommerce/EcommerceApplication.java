5ba315f03e4f369b5ae6c77d047678250120a297
Francisco Correia <felipedreamcorreia@hotmail.com>
2024-07-22 19:43:23 -0300

    feature: model app spring

---
+++ src/main/java/com/franciscode/ecommerce/EcommerceApplication.java
@@ -0,0 +1,13 @@
+package com.franciscode.ecommerce;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcommerceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EcommerceApplication.class, args);
+	}
+
+}
